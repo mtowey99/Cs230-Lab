f329fc5d5d8e889ba4bf920854d9556b1c5a31ca
mtowey99 <met0024@mix.wvu.edu>
2021-02-05 01:11:55 -0500

    Lab 2 about page

---
+++ index.php
@@ -0,0 +1,55 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About us</h1>
+        <p>evbhschvwehgiuwhdhkjxnbajkhasjlk</p>
+        </div>
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <img src="images/EliManning.jpeg">
+                    <div class="container">
+                        <h2>CEO</h2>
+                        <p style= "color: royalblue">Eli Manning</p>
+                        <p class="c1">Email: BradysFather10@superbowl.com</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="images/Cabbages.jpeg">
+                    <div class="container">
+                        <h2>Cabbage Expert</h2>
+                        <p>Cabbage Merchent</p>
+                        <p>Email: MyCabbagessss@Avatar.com</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="images/Aang.jpeg">
+                    <div class="container">
+                        <h2>Avatar</h2>
+                        <p>Aang</p>
+                        <p>Email: FlameoAang@Avatar.com</p>
+
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="card">
+                    <img src="images/Korra.jpeg">
+                    <div class="container">
+                        <h2>New Avatar</h2>
+                        <p>Korra</p>
+                        <p>Email: WhoNeedsAir@Avatar.com</p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</body>
