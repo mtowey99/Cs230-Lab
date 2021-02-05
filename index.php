d2901503e39b6db67ce3f0cf793eb9ed69c0b7f0
mtowey99 <met0024@mix.wvu.edu>
2021-02-05 01:35:00 -0500

    Lab 2 Updated

--- index.php
+++ index.php
@@ -2,50 +2,52 @@
     <link rel="stylesheet" href="css/about.css">
 
     <div class="about">
-        <h1>About us</h1>
-        <p>evbhschvwehgiuwhdhkjxnbajkhasjlk</p>
+        <h1>ABOUT US</h1>
+        <p style="color: green">Hello, this is our Team Avatar (ELi is only here for the SuperBowl). 
+            This group from across the Avatar Cinematic Universe will give you the best reviews on episodes and more. 
+            Remember there is no movie in Ba Sing Se.</p>
         </div>
         <div class="row">
             <div class="column">
-                <div class="card">
+                <div class="card1">
                     <img src="images/EliManning.jpeg">
                     <div class="container">
-                        <h2>CEO</h2>
+                        <h2 style = "color: royalblue">CEO</h2>
                         <p style= "color: royalblue">Eli Manning</p>
-                        <p class="c1">Email: BradysFather10@superbowl.com</p>
+                        <p style= "color: royalblue">Email: BradysFather10@superbowl.com</p>
 
                     </div>
                 </div>
             </div>
             <div class="column">
-                <div class="card">
+                <div class="card2">
                     <img src="images/Cabbages.jpeg">
                     <div class="container">
-                        <h2>Cabbage Expert</h2>
-                        <p>Cabbage Merchent</p>
-                        <p>Email: MyCabbagessss@Avatar.com</p>
+                        <h2 style = "color: darkgreen">Cabbage Expert</h2>
+                        <p style = "color: darkgreen">Cabbage Merchent</p>
+                        <p style = "color: darkgreen">Email: MyCabbagessss@Avatar.com</p>
 
                     </div>
                 </div>
             </div>
             <div class="column">
-                <div class="card">
+                <div class="card3">
                     <img src="images/Aang.jpeg">
                     <div class="container">
-                        <h2>Avatar</h2>
-                        <p>Aang</p>
-                        <p>Email: FlameoAang@Avatar.com</p>
+                        <h2 style = "color: #adababf5">Avatar</h2>
+                        <p style = "color: #adababf5">Aang</p>
+                        <p style = "color: #adababf5">Email: FlameoAang@Avatar.com</p>
 
                     </div>
                 </div>
             </div>
             <div class="column">
-                <div class="card">
+                <div class="card4">
                     <img src="images/Korra.jpeg">
                     <div class="container">
-                        <h2>New Avatar</h2>
-                        <p>Korra</p>
-                        <p>Email: WhoNeedsAir@Avatar.com</p>
+                        <h2 style = "color: #ff0101f5">New Avatar</h2>
+                        <p style = "color: #ff0101f5">Korra</p>
+                        <p style = "color: #ff0101f5">Email: WhoNeedsAir@Avatar.com</p>
 
                     </div>
                 </div>
